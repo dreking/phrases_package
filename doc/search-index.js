1c63f8a6772259883e67465757f4d7c1af9737c1
Leandre Nsengumuremyi <leajapo@gmail.com>
2023-04-21 19:39:27 +0200

    feat: added package documentattion

---
+++ doc/search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = JSON.parse('{\
+"french":{"doc":"French greetings","t":"FF","n":["goodbye","hello"],"q":["french",""],"d":["Returns a French farewell","Returns a French greeting"],"i":[0,0],"f":[[[],1],[[],1]],"p":[[3,"String"]]}\
+}');
+if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
+if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
